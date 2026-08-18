5cd2692e9b24b867ca1bef5ededa67525491e2be
Papan Banik <Papan.Banik@siarasolution.com>
2026-08-18 19:38:23 +0600

    fix : working address

--- src/section/Hero.jsx
+++ src/section/Hero.jsx
@@ -43,6 +43,23 @@ const Hero = () => {
   return (
     <div className="relative w-full sm:h-[500px] overflow-hidden overflow-x-hidden">
       {/* CONTENT */}
+      <div className="w-fit ml-4 sm:ml-40 mt-4 sm:mt-8  flex items-center gap-1 px-4 py-2  rounded-full border  border-[#213190] text-sm text-gray-300">
+        <span className="relative flex h-2.5 w-2.5 pt-1 ">
+          <motion.span
+            className="absolute w-1 h-1 rounded bg-cyan-400 "
+            animate={{
+              scale: [2, 2, 2],
+              opacity: [0.7, 0.4, 0.9],
+            }}
+            transition={{
+              duration: 1.8,
+              repeat: Infinity,
+              ease: "easeInOut",
+            }}
+          />
+        </span>
+        Available for new roles
+      </div>
       <div className="absolute z-20 top-20 left-0 w-full px-6 sm:pl-40 sm:pr-0 flex flex-col gap-3 text-white">
         <motion.h1
           initial={{ opacity: 0 }}
@@ -92,19 +109,29 @@ const Hero = () => {
       </div>
 
       {/* HERO IMAGE - DESKTOP */}
-      <div className="max-sm:hidden mt-10 relative z-10 flex justify-end pb-14 sm:pb-17 items-end sm:pr-40">
+
+      <div className="max-sm:hidden  mt-0 relative z-10 flex justify-end pb-14 sm:pb-17 items-end sm:pr-40">
+        <div className="absolute -top-6 translate-x-1 bg-[#1E2939] flex items-center gap-1 px-4 py-2  rounded-full border  border-[#213190] text-sm text-gray-300">
+          Based in Dhaka, Working Globally
+        </div>
         <motion.img
           initial={{ opacity: 0 }}
           animate={{ opacity: 1 }}
           transition={{ duration: 1.2 }}
           src={HeroImage}
           alt="Hero"
-          className="rounded sm:h-[400px] object-contain"
+          className="rounded  sm:h-[400px] object-contain"
         />
       </div>
 
       {/* HERO IMAGE - MOBILE */}
-      <div className="sm:hidden pt-[23rem] pb-[2rem]  relative z-10 flex justify-center items-end w-full">
+      <div className="sm:hidden pt-[22rem] pb-[2rem] relative z-10 flex justify-center items-end w-full">
+        <div className="relative">
+          {/* Location Badge */}
+          <div className="absolute left-10 -top-87 translate-x-1 bg-[#1E2939] flex items-center gap-1 px-4 py-2 rounded-full border border-[#213190] text-sm text-gray-300 whitespace-nowrap">
+            Based in Dhaka, Working Globally
+          </div>
+        </div>
         <motion.img
           initial={{ opacity: 0 }}
           animate={{ opacity: 1 }}
