036617e3eb91fe95e1a38f2b7269ad750059b9c3
Papan Banik <Papan.Banik@siarasolution.com>
2026-09-16 13:57:06 +0600

    fix : fix the resume section

--- src/section/PdfViewer.jsx
+++ src/section/PdfViewer.jsx
@@ -1,18 +1,17 @@
-import React from 'react';
-import CvFile from '../assets/Papan_Resume_SE.pdf'
-import Navbar from '../layout/Navbar';
+import React from "react";
+import CvFile from "../assets/Papan-SE-Resume.pdf";
+import Navbar from "../layout/Navbar";
 const PdfViewer = () => {
-    return (
-        <div className='w-full h-screen'>
-           <Navbar/>
-            <iframe
-            src={CvFile}
-            title='pdf viewer'
-              className="w-full h-full"
-            >
-            </iframe>
-        </div>
-    );
+  return (
+    <div className="w-full h-screen">
+      <Navbar />
+      <iframe
+        src={CvFile}
+        title="pdf viewer"
+        className="w-full h-full"
+      ></iframe>
+    </div>
+  );
 };
 
 export default PdfViewer;
